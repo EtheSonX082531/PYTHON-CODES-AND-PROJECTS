f19aa80ed3e3d53525739c8e539112a5f7ddb1f4
Anik Roy <136085433+EtheSonX082531@users.noreply.github.com>
2025-11-20 17:09:19 +0600

    Update Basic_Student_Class.py

--- OOPS/Basic_Student_Class.py
+++ OOPS/Basic_Student_Class.py
@@ -1,20 +1,21 @@
 class Student:
     school_name="Govt Agragami School And College"
-    def __init__(self,name,marks1,marks2,marks3):
+    def __init__(self,name,marks):
         self.name=name
-        self.marks1=marks1
-        self.marks2=marks2
-        self.marks3=marks3
+        self.marks=marks
     
     def average(self):
-        avg=(self.marks1+self.marks2+self.marks3)//3
+        total_marks=0
+        for mark in self.marks:
+            total_marks+=mark
+        avg=total_marks//3    
         print(f"Average Marks of {self.name} is:",avg)  
         
 
 print(Student.school_name,"\n")
         
-s1=Student("Smita Paramita",90,85,100)
+s1=Student("Smita Paramita",[90,85,100])
 s1.average() 
 
-s2=Student("Marian Chowdhury",98,92,80)
+s2=Student("Marian Chowdhury",[98,92,80])
 s2.average()
