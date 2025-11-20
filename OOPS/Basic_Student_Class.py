c38b7fba834ca9ab501bb4c464718611cf84f744
Anik Roy <136085433+EtheSonX082531@users.noreply.github.com>
2025-11-20 17:48:56 +0600

    Enhance Student class with static method and count

--- OOPS/Basic_Student_Class.py
+++ OOPS/Basic_Student_Class.py
@@ -1,8 +1,10 @@
 class Student:
     school_name="Govt Agragami School And College"
+    stu_num = 0
     def __init__(self,name,marks):
         self.name=name
         self.marks=marks
+        Student.stu_num+=1
     
     def average(self):
         total_marks=0
@@ -11,11 +13,19 @@ class Student:
         avg=total_marks//3    
         print(f"Average Marks of {self.name} is:",avg)  
         
+    @staticmethod
+    def notice():
+        print("This is the Notice Board🔔")
+        
 
 print(Student.school_name,"\n")
         
 s1=Student("Smita Paramita",[90,85,100])
 s1.average() 
+print("Total Student:",Student.stu_num)
 
 s2=Student("Marian Chowdhury",[98,92,80])
 s2.average()
+print("Total Student:",Student.stu_num)
+
+Student.notice()
